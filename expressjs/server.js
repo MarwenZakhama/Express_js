66ea13ac0bb5b0c32ffa934bd517c10fb50898ae
MarwenZakhama <103939628+MarwenZakhama@users.noreply.github.com>
2022-07-29 14:44:45 +0100

    1.0

---
+++ expressjs/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+app.set('view engine', 'pug');
+app.set('views','./pages');
+const checkTime = (req, res, next) => {
+    var d=new Date().getDay()
+    var h=new Date().getHours()
+    if((h >= 9 && h<18) && (d > 0 && d <6)){
+        next();
+    }else{
+        res.send("not found")
+    }
+}
+app.use(checkTime)
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/views/Home.html')
+    // res.render('home');
+})
+app.get('/contact', (req, res)=>{
+    res.sendFile(__dirname + '/views/Contactus.html')
+})
+app.get('/services', (req, res)=>{
+    res.sendFile(__dirname + '/views/OurServices.html')
+})
+app.use(express.static('views/css'))
+app.listen(5000,() => {console.log("server is running");});
